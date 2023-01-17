e02c066d4fa7262fc434d693d46773a4ae9df877
FIN-THE-HUMAN <malyshevi@yandex.ru>
2023-01-17 19:53:34 +0300

    Сделан предварительный AI противника
    Пока имеется баг с отталкиванием противника от стен

---
+++ Assets/Scripts/Enemy/EnemyAI.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class EnemyAI : MonoBehaviour
+{
+    [SerializeField] private Transform _player;
+    private NavMeshAgent _navMeshAgent;
+
+    void Start()
+    {
+        _navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _navMeshAgent.SetDestination(_player.position);
+    }
+}
